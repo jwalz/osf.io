d0d119d23737c547de5fee1d41df13d1df9cfd6c
Jon Walz <jonwalz@Admins-MBP.lan>
2022-09-12 09:04:13 -0400

    explicitly clear project cache

--- osf_tests/test_files.py
+++ osf_tests/test_files.py
@@ -82,10 +82,7 @@ def test_file_update_respects_region(project, user, create_test_file):
     new_region = RegionFactory()
     node_settings.region = new_region
     node_settings.save()
-    test_file.save()
-    cached_project = test_file.target
-    cached_project.refresh_from_db()
-#    test_file.reload()
+    del project.osfstorage_region
 
     new_version = test_file.create_version(
         user, {
